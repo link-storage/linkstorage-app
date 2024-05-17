e31255b355e45732f0d7d9be39f87052eddf8297
resister-boy <hassanpumped17@gmail.com>
2024-05-17 22:47:51 +0900

    config: add babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@/': './src',
+          '@/assets': './assets',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
