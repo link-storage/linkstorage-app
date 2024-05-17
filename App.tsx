4525977c225d67977df7ca3b41853ed4ca6c0971
resister-boy <hassanpumped17@gmail.com>
2024-05-17 22:46:34 +0900

    feat: add base app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { NavigationContainer, NavigationContainerRef, Route } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import store from '@/redux/store';
+import StackNavigator from '@/screens/StackNavigator';
+import { MMKV } from 'react-native-mmkv';
+import { firebase } from '@react-native-firebase/analytics';
+import { StackParamList } from '@/types/routes';
+import analytics from '@react-native-firebase/analytics';
 
-const App = () => {
+export const storage = new MMKV();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 1 },
+    mutations: { retry: 1 },
+  },
+});
+
+GoogleSignin.configure({
+  webClientId: "",
+  iosClientId: ""
+});
+
+export default function App() {
+  const navigationRef = useRef<NavigationContainerRef<StackParamList> | null>(null);
+  const [routeNameRef, setRefNameRef] = useState<Route<string> | undefined>(undefined)
+
+  const handleSetRoute = useCallback(async () => {
+    const current = navigationRef.current?.getCurrentRoute();
+    const previousRoute = routeNameRef?.name;
+
+    const currentRoute = navigationRef.current?.getCurrentRoute()?.name;
+
+    if (previousRoute !== currentRoute) {
+      await analytics().logScreenView({});
+    }
+  }, [routeNameRef])
+
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+
+  }, []);
+
+
+  useEffect(() => {
+    const activateAnalytics = async() => {
+      await firebase.analytics().setAnalyticsCollectionEnabled(true);
+    }
+    activateAnalytics();
+  }, [])
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer 
+      ref={navigationRef}
+      onReady={() => {
+        setRefNameRef(navigationRef.current?.getCurrentRoute());
+        handleSetRoute();
+      }}
+      onStateChange={() => {
+        handleSetRoute().then(() => {
+          setRefNameRef(navigationRef.current?.getCurrentRoute())
+        });
+      }}
+      >
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <StackNavigator />
+        </Provider>
+      </QueryClientProvider>
+    </NavigationContainer>
+  );
 }
-
-export default App
-
-const styles = StyleSheet.create({})
