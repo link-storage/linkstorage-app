f0a6c945052c60deb09f325af029183c51b3e780
resister-boy <hassanpumped17@gmail.com>
2024-07-29 01:29:57 +0900

    feat: declare step screen

---
+++ src/screen/step/sign/SignUpBaseStep.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignUpBaseStep = () => {
+  return (
+    <View>
+      <Text>SignUpBaseStep</Text>
+    </View>
+  )
+}
+
+export default SignUpBaseStep
+
+const styles = StyleSheet.create({})
---
+++ src/screen/step/sign/SignUpProfileStep.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignUpProfileStep = () => {
+  return (
+    <View>
+      <Text>SignUpProfileStep</Text>
+    </View>
+  )
+}
+
+export default SignUpProfileStep
+
+const styles = StyleSheet.create({})
